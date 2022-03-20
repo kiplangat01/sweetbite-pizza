cf616658eb33c658c38f02670d38e8eac68aa75b
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-03-20 20:35:18 +0300

    made the entire page interactive using javascript

--- script.js
+++ script.js
@@ -1,2 +1,87 @@
+function Order(pizza, toppings, crust){
+    this.pizza=pizza;
+    this.toppings=toppings;
+    this.crust=crust;
+}
+$(document).ready(function(){
+    
 
-            
+    $("#deliver").click(function(){
+
+        $(".yes").show()
+        $("#formpizza").hide()
+
+        function myPizza(){
+            const pizzaCost = document.getElementById("pizza").value;
+            return parseInt(pizzaCost);
+        }
+        function myToppings(){
+            const toppingsCost = document.getElementById("toppings").value;
+            return parseInt(toppingsCost);
+        }
+        function myCrust(){
+            const crustCost = document.getElementById("crust").value;
+            return parseInt(crustCost);
+        }
+        let cost =  new Order(myPizza(), myToppings(), myCrust())
+        
+        var totalCost = cost.pizza + cost.toppings + cost.crust 
+        // alert(totalCost)
+    
+       
+        $("#ok").append("your total ammount is"  + " " + totalCost );
+        
+    })
+    $("#no").click(function(){
+        $("#ok").show()
+        $(".yes").hide()
+        
+    })
+    $("#yes").click(function(){
+        $("#ok").hide()
+        $("#form2").show()
+        $(".yes").hide()
+
+        
+    })
+
+    $("#finalorder").click(function(){
+        $("#ok").hide()
+        $("#form2").show()
+        $(".yes").hide()
+        $("#yes").hide()
+         
+        function myPizza(){
+            const pizzaCost = document.getElementById("pizza").value;
+            return parseInt(pizzaCost);
+        }
+        function myToppings(){
+            const toppingsCost = document.getElementById("toppings").value;
+            return parseInt(toppingsCost);
+        }
+        function myCrust(){
+            const crustCost = document.getElementById("crust").value;
+            return parseInt(crustCost);
+        }
+        let cost =  new Order(myPizza(), myToppings(), myCrust())
+        
+        var totalCost = cost.pizza + cost.toppings + cost.crust 
+
+        var newCost = totalCost + 300 
+        
+        let customer = $("input#name").val();
+            let phone = $("input#phone").val();
+            let area = $("input#place").val();
+    
+            if ($("input#name").val() && $("input#phone").val() && $("input#place").val()!=""){
+               alert ("hello" + " " + customer + "we have received your order your total cost is" + " " 
+               + newCost + "the product will be delivered to" + area)
+              }
+              else {
+                alert("Please fill in the details for delivery!");
+               
+              }
+        
+
+    })
+})
